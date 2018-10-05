47211fca95781065e2ffa28513f3dffe20d42fa5
Jason Stajich <jason.stajich@gmail.com>
2018-10-05 16:38:45 -0700

    problem 3 solved

--- solution1.sh
+++ solution1.sh
@@ -7,3 +7,16 @@ du -h Ecoli-vs-Yersinia.BLASTP.tab.gz
 # how big is the uncompressed file
 gunzip Ecoli-vs-Yersinia.BLASTP.tab.gz
 du -h Ecoli-vs-Yersinia.BLASTP.tab
+
+# print out first 25
+
+head -n 25 Ecoli-vs-Yersinia.BLASTP.tab
+
+# print out the last 3
+
+tail -n 3 Ecoli-vs-Yersinia.BLASTP.tab
+
+# print out the number of lines
+
+wc -l Ecoli-vs-Yersinia.BLASTP.tab
+
